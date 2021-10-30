67f3f75fecc9a0cdaa47f4c23c3a8c2331e7aab6
prasanjitsarker3 <sarkerprasanjit379@gmail.com>
2021-10-31 01:01:33 +0600

    pizza

---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar__text">
+           
+        <ul className="navbar__ul">
+          <li>
+            <a href="home">Home</a>
+          </li>
+          <li>
+            <a href="menu">Menu</a>
+          </li>
+          <li>
+            <a href="shop">Shop</a>
+          </li>
+          <li>
+            <a href="news">News</a>
+          </li>
+          <li>
+            <a href="contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
